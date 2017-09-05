d01df002eb884c33872618db4c2344aefdbdfa5e
Jondy Zhao <jondy.zhao@gmail.com>
2017-09-05 15:34:23 +0800

    Refine importer

--- src/pyimcore.py
+++ src/pyimcore.py
@@ -1,5 +1,4 @@
-# It seems imp must be imported even if there is no used in module
-import imp
+from imp import find_module, load_module, new_module, PKG_DIRECTORY
 import os
 import sys
 
@@ -9,28 +8,57 @@ ext_char = os.getenv('PYARMOR_EXTRA_CHAR', 'e')
 ext_list = [x + ext_char for x in ('.py', '.pyc', '.pyo')]
 
 class PyshieldImporter(object):
+    '''Import encrypted module or package, package in multi-pathes is not supported.'''
 
     def __init__(self):
-        self.filename = ''
+        self.mod_info = None
+        self.imp_loader = False
 
-    def find_module(self, fullname, path=None):
-        _name = fullname.rsplit('.', 1)[-1]
+    def find_module(self, name, path=None):
+        try:
+            self.mod_info = find_module(name, path=path)
+            self.imp_loader = True
+            return self
+        except ImportError:
+            self.imp_loader = False
+
+        m = name.rsplit('.', 1)[-1]
         for dirname in sys.path if path is None else path:
             for ext in ext_list:
-                self.filename = os.path.join(dirname, _name + ext)
-                if os.path.exists(self.filename):
+                filename = os.path.join(dirname, m + ext)
+                if os.path.exists(filename):
+                    self.mod_info = None, filename, None
                     return self
-        self.filename = ''
+                filename = os.path.join(dirname, name, '__init__' + ext)
+                if os.path.exists(filename):
+                    self.mod_info = None, filename, PKG_DIRECTORY
+                    return self
+        self.mod_info = None
 
-    def load_module(self, fullname):
-        ispkg = 0
-        try:
-            mod = import_module(fullname, self.filename)
-            mod.__file__ = '<pytransform>'
-            mod.__loader__ = self
-        except Exception:
-            raise ImportError('Import module from %s failed' % fullname)
-        return mod
+    def load_module(self, name):
+        fp, filename, description = self.mod_info
+        if self.imp_loader is None:
+            m = import_module(name, filename)
+            m.__loader__ = self
+            if description == PKG_DIRECTORY:
+                m.__package__ = name
+                m.__path__ = [os.path.dirname(filename)]
+        else:
+            m = load_module(name, fp, filename, description)
+        if not description == PKG_DIRECTORY:
+            i = name.rfind('.')
+            if not i == -1:
+                m.__package__ = name[:i]
+        return m
+
+    def load_package(self, name, filenames):
+        pkg = new_module(name)
+        path = []
+        for filename in filenames:
+            m = import_module(name, filename)
+            path.append(os.path.dirname(filename))
+        pkg.__path__ = path
+        return pkg
 
 sys.meta_path.append(PyshieldImporter())
 init_runtime()
