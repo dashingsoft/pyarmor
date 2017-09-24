cc1b0016a5ea9ebcadbc172574da4884e4405cfa
Jondy Zhao <jondy.zhao@gmail.com>
2017-09-24 10:38:22 +0800

    Add doc to webapp distribute

--- webapp/build-webapp.sh
+++ webapp/build-webapp.sh
@@ -11,10 +11,12 @@ target=$build/pyarmor/webapp
 cp *.py *.html *.js *.md $target
 cp -a js css images $target
 cp start-server.* $target
-cp $src/*.py $src/*.key $src/*.lic $target/$src
+cp $src/*.py $src/*.key $src/*.lic $src/*.md $src/*.html $target/$src
 cp -a $src/platforms $target/$src
 cp -a $src/examples $target/$src
 rm $target/$src/setup.py
+
+cp ../ChangeLog.rst ../README.* $target/..
 
 (cd $build && zip -r ../pyarmor-webapp ./)
 
