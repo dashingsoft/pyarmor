adf6e6353c813307603eee71e7dbdc6bcba74150
Jondy Zhao <jondy.zhao@gmail.com>
2018-01-22 05:52:21 +0800

    Add system testcases

---
+++ tests/system-test.sh
@@ -0,0 +1,298 @@
+source test-header.sh
+
+# ======================================================================
+#
+# Initial setup.
+#
+# ======================================================================
+
+PYARMOR="${PYTHON} pyarmor2.py"
+
+csih_inform "Python is $PYTHON"
+csih_inform "Tested Package: $pkgfile"
+csih_inform "Pyarmor is $PYARMOR"
+
+csih_inform "Make workpath ${workpath}"
+rm -rf ${workpath}
+mkdir -p ${workpath} || csih_error "Make workpath FAILED"
+
+cd ${workpath}
+[[ ${pkgfile} == *.zip ]] && unzip ${pkgfile} > /dev/null 2>&1
+[[ ${pkgfile} == *.tar.bz2 ]] && tar xjf ${pkgfile}
+cd pyarmor-$version || csih_error "Invalid pyarmor package file"
+
+csih_inform "Prepare for system testing"
+echo ""
+
+
+# ======================================================================
+#
+#  Bootstrap: help and version
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Bootstrap ---------------------------------"
+echo ""
+
+csih_inform "Case 0.1: show help and import pytransform"
+$PYARMOR --help >result.log 2>&1 || csih_bug "Case 1.1 FAILED"
+[[ -f _pytransform$DLLEXT ]] || csih_error "no pytransform extension found"
+
+csih_inform "Case 0.2: show version information"
+$PYARMOR --version >result.log 2>&1 || csih_bug "show version FAILED"
+
+echo ""
+echo "-------------------- Bootstrap End -----------------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: obfuscate
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command obfuscate --------------------"
+echo ""
+
+csih_inform "Case 1.1: obfuscate script"
+$PYARMOR obfuscate --src examples --entry queens.py --output dist \
+                   "*.py"  >result.log 2>&1
+
+check_file_exists dist/queens.py
+check_file_content dist/queens.py '__pyarmor__(__name__'
+
+( cd dist; $PYTHON queens.py >result.log 2>&1 )
+check_file_content dist/result.log 'Found 92 solutions'
+
+csih_inform "Case 1.2: obfuscate module"
+$PYARMOR obfuscate --src examples/py2exe --entry hello.py \
+                   --output dist2 queens.py >result.log 2>&1
+
+check_file_exists dist2/hello.py
+check_file_content dist2/hello.py 'pyarmor_runtime'
+
+check_file_exists dist2/queens.py
+check_file_content dist2/queens.py '__pyarmor__(__name__'
+
+(cd dist2; $PYTHON hello.py >result.log 2>&1 )
+check_file_content dist2/result.log 'Found 92 solutions'
+
+echo ""
+echo "-------------------- Test Command obfuscate END ----------------"
+echo ""
+
+
+# ======================================================================
+#
+#  Command: init
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command init -------------------------"
+echo ""
+
+csih_inform "Case 2.1: init pybench"
+$PYARMOR init --src examples/pybench --entry pybench.py \
+              projects/pybench >result.log 2>&1
+
+check_file_exists projects/pybench/.pyarmor_config
+check_file_exists projects/pybench/.pyarmor_capsule.zip
+
+csih_inform "Case 2.1: init py2exe"
+$PYARMOR init --src examples/py2exe --entry "hello.py,setup.py" \
+              projects/py2exe >result.log 2>&1
+
+check_file_exists projects/py2exe/.pyarmor_config
+check_file_exists projects/py2exe/.pyarmor_capsule.zip
+
+echo ""
+echo "-------------------- Test Command init END ---------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: config
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command config -----------------------"
+echo ""
+
+csih_inform "Case 3.1: config py2exe"
+( cd projects/py2exe; $ARMOR config --runtime-path='' >result.log 2>&1 )
+check_return_value
+
+echo ""
+echo "-------------------- Test Command config END -------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: info
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command info -------------------------"
+echo ""
+
+csih_inform "Case 4.1: info pybench"
+( cd projects/pybench; $ARMOR info >result.log 2>&1 )
+check_return_value
+
+csih_inform "Case 4.2: info py2exe"
+( cd projects/py2exe; $ARMOR info >result.log 2>&1 )
+check_return_value
+
+echo ""
+echo "-------------------- Test Command info END ---------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: check
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command check ------------------------"
+echo ""
+
+csih_inform "Case 5.1: check pybench"
+( cd projects/pybench; $ARMOR check >result.log 2>&1 )
+check_return_value
+
+csih_inform "Case 5.2: check py2exe"
+( cd projects/py2exe; $ARMOR check >result.log 2>&1 )
+check_return_value
+
+echo ""
+echo "-------------------- Test Command check END --------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: build
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command build ------------------------"
+echo ""
+
+csih_inform "Case 6.1: build pybench"
+( cd projects/pybench; $ARMOR build >result.log 2>&1 )
+
+output=projects/pybench/dist
+check_file_exists $output/pybench.py
+check_file_content $output/pybench.py 'pyarmor_runtime()'
+check_file_content $output/pybench.py '__pyarmor__(__name__'                                    
+
+echo ""
+echo "-------------------- Test Command build END --------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: licenses
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command licenses ---------------------"
+echo ""
+
+csih_inform "Case 7.1: Generate project licenses"
+output=projects/pybench/licenses
+
+( cd projects/pybench; $ARMOR licenses code1 code2 code3 \
+                       >licenses-result.log 2>&1 )
+check_file_exists $output/code1/license.lic
+check_file_exists $output/code2/license.lic
+check_file_exists $output/code3/license.lic
+check_file_exists $output/code1/license.lic.txt
+
+( cd projects/pybench; $ARMOR licenses \
+                              --expired $(next_month) \
+                              --bind-disk '${harddisk_sn}' \
+                              --bind-ipv4 '${ifip_address}' \
+                              --bind-mac '${ifmac_address}' \
+                              customer-tom >licenses-result.log 2>&1 )
+check_file_exists $output/customer-tom/license.lic
+check_file_exists $output/customer-tom/license.lic.txt
+
+echo ""
+echo "-------------------- Test Command licenses END -----------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: hdinfo
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command hdinfo -----------------------"
+echo ""
+
+csih_inform "Case 8.1: show hardware info"
+$PYARMOR hdinfo >result.log 2>&1
+check_return_value
+
+echo ""
+echo "-------------------- Test Command hdinfo END -------------------"
+echo ""
+
+# ======================================================================
+#
+#  Command: benchmark
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Command benchmark --------------------"
+echo ""
+
+csih_inform "Case 9.1: run benchmark test"
+for obf_module_mode in none des ; do
+  csih_inform "obf_module_mode: $obf_module_mode"
+  for obf_code_mode in none des fast ; do
+    csih_inform "obf_code_mode: $obf_code_mode"
+    logfile="result_${obf_module_mode}_${obf_code_mode}.log"
+    $PYARMOR benchmark --obf-module-mode $obf_module_mode \
+                       --obf-code-mode $obf_code_mode \
+                       >$logfile 2>&1
+    check_return_value
+    csih_inform "Write benchmark test results to $logfile"
+    check_file_content $logfile "run_ten_thousand_obfuscated_bytecode"
+    rm -rf .benchtest
+  done
+done
+
+echo ""
+echo "-------------------- Test Command benchmark END ----------------"
+echo ""
+
+# ======================================================================
+#
+# Finished and cleanup.
+#
+# ======================================================================
+
+# Return test root
+cd ../..
+
+echo "----------------------------------------------------------------"
+echo ""
+csih_inform "Test finished for ${PYTHON}"
+
+(( ${_bug_counter} == 0 )) || csih_error "${_bug_counter} bugs found"
+echo "" && \
+csih_inform "Remove workpath ${workpath}" \
+&& echo "" \
+&& rm -rf ${workpath} \
+&& csih_inform "Congratulations, there is no bug found"
---
+++ tests/test-header.sh
@@ -0,0 +1,347 @@
+#! /bin/bash
+
+UNAME=$(uname)
+if [[ ${UNAME:0:5} == Linux ]] ; then
+    if [[ $(arch) == x86_64 ]] ; then
+        PLATFORM=linux_x86_64
+    else
+        PLATFORM=linux_i386
+    fi
+    PKGEXT=tar.bz2
+    DLLEXT=.so
+    ARMOR=./pyarmor
+else
+
+    if [[ $(uname) == Darwin ]] ; then
+        PLATFORM=macosx_intel
+        PKGEXT=tar.bz2
+        DLLEXT=.so
+        ARMOR=./pyarmor
+    else
+        if [[ $(arch) == x86_64 ]] ; then
+            PLATFORM=win_amd64
+        else
+            PLATFORM=win32
+        fi
+        PKGEXT=zip
+        DLLEXT=.dll
+        ARMOR=./pyarmor.bat
+    fi
+fi
+
+# version=${1:-2.6.1}
+filename=$(cd ../src/dist; ls -t pyarmor-*.${PKGEXT}) || exit 1
+version=${filename:8:5}
+workpath=__runtest2__
+pkgfile=$(pwd)/../src/dist/pyarmor-${version}.${PKGEXT}
+
+declare -i _bug_counter=0
+
+case ${PLATFORM} in
+
+    win32)
+        PYTHON=${PYTHON:-C:/Python26/python}
+        declare -r harddisk_sn=100304PBN2081SF3NJ5T
+        declare -r ifmac_address=70:f1:a1:23:f0:94
+        declare -r ifip_address=192.168.121.101
+        declare -r domain_name=
+        ;;
+    win_amd64)
+        PYTHON=${PYTHON:-C:/Python26/python}
+        declare -r harddisk_sn=100304PBN2081SF3NJ5T
+        declare -r ifmac_address=70:f1:a1:23:f0:94
+        declare -r ifip_address=169.254.225.168
+        declare -r domain_name=
+        ;;
+    linux_i386)
+        PYTHON=${PYTHON:-python}
+        declare -r harddisk_sn='VB07ab3ff6-81eb5787 '
+        declare -r ifmac_address=08:00:27:88:4b:88
+        declare -r ifip_address=192.168.121.106
+        declare -r domain_name=
+        ;;
+    linux_x86_64)
+        PYTHON=${PYTHON:-python}
+        declare -r harddisk_sn='            9WK3FEMQ'
+        declare -r ifmac_address='00:23:8b:e0:4f:a7'
+        declare -r ifip_address=192.168.121.103
+        declare -r domain_name=
+        ;;
+    macosx_intel)
+        PYTHON=python
+        declare -r harddisk_sn='VB85de09d4-23402b07 '
+        declare -r ifmac_address=08:00:27:b0:b3:94
+        declare -r ifip_address=220.250.64.20
+        declare -r domain_name=
+        ;;
+    *)
+        echo Unknown platform "${PLATFORM}"
+        exit 1
+    esac
+
+# ======================================================================
+# csih routines
+# ======================================================================
+
+_csih_trace=
+
+_csih_ERROR_STR_COLOR="\e[1;31m*** ERROR:\e[0;0m"
+_csih_WARN_STR_COLOR="\e[1;33m*** Warning:\e[0;0m"
+_csih_INFO_STR_COLOR="\e[1;32m*** Info:\e[0;0m"
+_csih_QUERY_STR_COLOR="\e[1;35m*** Query:\e[0;0m"
+_csih_STACKTRACE_STR_COLOR="\e[1;36m*** STACKTRACE:\e[0;0m"
+readonly _csih_ERROR_STR_COLOR _csih_WARN_STR_COLOR
+readonly _csih_INFO_STR_COLOR _csih_QUERY_STR_COLOR _csih_STACKTRACE_STR_COLOR
+
+_csih_ERROR_STR_PLAIN="*** ERROR:"
+_csih_WARN_STR_PLAIN="*** Warning:"
+_csih_INFO_STR_PLAIN="*** Info:"
+_csih_QUERY_STR_PLAIN="*** Query:"
+_csih_STACKTRACE_STR_PLAIN="*** STACKTRACE:"
+readonly _csih_ERROR_STR_PLAIN _csih_WARN_STR_PLAIN
+readonly _csih_INFO_STR_PLAIN _csih_QUERY_STR_PLAIN _csih_STACKTRACE_STR_PLAIN
+
+_csih_ERROR_STR="${_csih_ERROR_STR_COLOR}"
+_csih_WARN_STR="${_csih_WARN_STR_COLOR}"
+_csih_INFO_STR="${_csih_INFO_STR_COLOR}"
+_csih_QUERY_STR="${_csih_QUERY_STR_COLOR}"
+_csih_STACKTRACE_STR="${_csih_STACKTRACE_STR_COLOR}"
+
+# ======================================================================
+# Routine: csih_disable_color
+#   Provided so that scripts which are invoked via postinstall
+#   can prevent escape codes from showing up in /var/log/setup.log
+# ======================================================================
+csih_disable_color()
+{
+  _csih_ERROR_STR="${_csih_ERROR_STR_PLAIN}"
+  _csih_WARN_STR="${_csih_WARN_STR_PLAIN}"
+  _csih_INFO_STR="${_csih_INFO_STR_PLAIN}"
+  _csih_QUERY_STR="${_csih_QUERY_STR_PLAIN}"
+  _csih_STACKTRACE_STR="${_csih_STACKTRACE_STR_PLAIN}"
+} # === End of csih_disable_color() === #
+readonly -f csih_disable_color
+
+# ======================================================================
+# Routine: csih_stacktrace
+# ======================================================================
+csih_stacktrace()
+{
+  set +x # don''t trace this!
+  local -i n=$(( ${#FUNCNAME} - 1 ))
+  local val=""
+  if [ -n "$_csih_trace" ]
+  then
+    while [ $n -gt 0 ]
+    do
+      if [ -n "${FUNCNAME[$n]}" ]
+      then
+        if [ -z "$val" ]
+        then
+          val="${FUNCNAME[$n]}[${BASH_LINENO[$(($n-1))]}]"
+        else
+          val="${val}->${FUNCNAME[$n]}[${BASH_LINENO[$(($n-1))]}]"
+        fi
+      fi
+    n=$(($n-1))
+    done
+    echo -e "${_csih_STACKTRACE_STR} ${val} ${@}"
+  fi
+} # === End of csih_stacktrace() === #
+readonly -f csih_stacktrace
+
+# ======================================================================
+# Routine: csih_trace_on
+#   turns on shell tracing of csih functions
+# ======================================================================
+csih_trace_on()
+{
+  _csih_trace='set -x'
+  trap 'csih_stacktrace "returning with" $?; set -x' RETURN
+  set -T
+  csih_stacktrace "${@}"
+} # === End of csih_trace_on() === #
+readonly -f csih_trace_on
+
+# ======================================================================
+# Routine: csih_trace_off
+#   turns off shell tracing of csih functions
+# ======================================================================
+csih_trace_off()
+{
+  trap '' RETURN
+  csih_stacktrace "${@}"
+  _csih_trace=
+  set +x
+  set +T
+} # === End of csih_trace_off() === #
+readonly -f csih_trace_off
+
+# ======================================================================
+# Routine: csih_error
+#   Prints the (optional) error message $1, then
+#   Exits with the error code contained in $? if $? is non-zero, otherwise
+#     exits with status 1
+#   All other arguments are ignored
+# Example: csih_error "missing file"
+# NEVER RETURNS
+# ======================================================================
+csih_error()
+{
+  local errorcode=$?
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  if ((errorcode == 0))
+  then
+    errorcode=1
+  fi
+  echo -e "${_csih_ERROR_STR} ${1:-no error message provided}"
+  exit ${errorcode};
+} # === End of csih_error() === #
+readonly -f csih_error
+
+# ======================================================================
+# Routine: csih_error_multi
+#   Prints the (optional) error messages in the positional arguments, one
+#     per line, and then
+#   Exits with the error code contained in $? if $? is non-zero, otherwise
+#     exits with status 1
+#   All other arguments are ignored
+# Example: csih_error_multi "missing file" "see documentation"
+# NEVER RETURNS
+# ======================================================================
+csih_error_multi()
+{
+  local errorcode=$?
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  if ((errorcode == 0))
+  then
+    errorcode=1
+  fi
+  while test $# -gt 1
+  do
+    echo -e "${_csih_ERROR_STR} ${1}"
+    shift
+  done
+  echo -e "${_csih_ERROR_STR} ${1:-no error message provided}"
+  exit ${errorcode};
+} # === End of csih_error_multi() === #
+readonly -f csih_error_multi
+
+# ======================================================================
+# Routine: csih_error_recoverable
+#   Prints the supplied errormessage, and propagates the $? value
+# Example: csih_error_recoverable "an error message"
+# ======================================================================
+csih_error_recoverable()
+{
+  local errorcode=$?
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  echo -e "${_csih_ERROR_STR} ${1}"
+  $_csih_trace
+  return $errorcode
+} # === End of csih_error_recoverable() === #
+readonly -f csih_error_recoverable
+
+
+# ======================================================================
+# Routine: csih_warning
+#   Prints the supplied warning message
+# Example: csih_warning "replacing default file foo"
+# ======================================================================
+csih_warning()
+{
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  echo -e "${_csih_WARN_STR} ${1}"
+  $_csih_trace
+} # === End of csih_warning() === #
+readonly -f csih_warning
+
+# ======================================================================
+# Routine: csih_inform
+#   Prints the supplied informational message
+# Example: csih_inform "beginning dependency analysis..."
+# ======================================================================
+csih_inform()
+{
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  echo -e "${_csih_INFO_STR} ${1}"
+  $_csih_trace
+} # === End of csih_inform() === #
+readonly -f csih_inform
+
+# ======================================================================
+# Routine: csih_bug
+#   Prints the supplied warning message and increase bug counter
+# ======================================================================
+csih_bug()
+{
+  let _bug_counter++
+  set +x # don't trace this, but we are interested in who called
+  csih_stacktrace # we'll see the arguments in the next statement
+  echo -e "${_csih_WARN_STR} ${1}"
+  $_csih_trace
+} # === End of csih_bug() === #
+readonly -f csih_bug
+
+# ======================================================================
+# Routine: next_month
+#   Print next month
+# ======================================================================
+next_month()
+{
+  let -i _year=10#$(date +%Y)
+  let -i _month=10#$(date +%m)
+  if (( _month == 12 )) ; then
+      let _year++
+      let _month=1
+  else
+      let _month++
+  fi
+  if (( _month > 9 )) ; then
+      month="${_month}"
+  else
+      month="0${_month}"
+  fi
+  echo -e "${_year}-${month}-01"
+} # === End of next_month() === #
+readonly -f next_month
+
+
+# ======================================================================
+# Routine: check_file_exists
+#   Write a bug if file doesn't exists
+# Example: check_file_exists dist/foo.py
+# ======================================================================
+check_file_exists()
+{
+  [[ -f "$1" ]] || csih_bug "no $1 found"
+}
+
+# ======================================================================
+# Routine: check_file_content
+#   Write a bug if file doesn't include some content
+# Example: check_file_content result.log "__pyarmor__"
+# ======================================================================
+check_file_content()
+{
+    if [[ "$3" == "not" ]] ; then
+        grep -q "$2" "$1" && csih_bug "'$2' found in '$1'"
+    else
+        grep -q "$2" "$1" || csih_bug "'$2' not found in '$1'"
+    fi
+}
+
+# ======================================================================
+# Routine: check_return_value
+#   Write a bug  if return value is not 0
+# Example: check_return_value
+# ======================================================================
+check_return_value()
+{
+    (( $? )) && csih_bug "command return non-zero value"
+}
