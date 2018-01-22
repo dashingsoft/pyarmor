1f3a61866195b6d4f0de955400c758c8465d721b
Jondy Zhao <jondy.zhao@gmail.com>
2018-01-22 09:09:15 +0800

    Remove unused command

--- build-package.sh
+++ build-package.sh
@@ -1,7 +1,3 @@
-# Upload dist\pyarmor-%1.tar.bz2 to pypi
-# Note that it need run on window's command box
-# C:\Python34\Scripts\twine upload dist\pyarmor-%1.tar.bz2
-
 # Build wheel
 # C:/Python34/python setup.py --bdist_wheel \
 #      --plat-name=win32 --python-tag=py2.py3
