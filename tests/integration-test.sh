70be07505f2ba1524de6ee5f61698e2b9a022a0f
Jondy Zhao <jondy.zhao@gmail.com>
2018-02-07 11:58:06 +0800

    Get ip before run testcase in macos

--- tests/integration-test.sh
+++ tests/integration-test.sh
@@ -67,7 +67,7 @@ case ${PLATFORM} in
         PYTHON=python
         declare -r harddisk_sn='VB85de09d4-23402b07 '
         declare -r ifmac_address=08:00:27:b0:b3:94
-        declare -r ifip_address=220.250.64.20
+        declare -r ifip_address=$(ipconfig getifaddr en0)
         declare -r domain_name=
         ;;
     *)
