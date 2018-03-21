66263783dd6cd8579f4d33526f7dec61d2191de9
Jondy Zhao <jondy.zhao@gmail.com>
2018-03-21 12:51:47 +0800

    Change testcase for wraparmor

--- src/examples/testmod/hello.py
+++ src/examples/testmod/hello.py
@@ -8,32 +8,28 @@ from queens import main, Queens
 # Call obfuscated function
 main()
 
-# Check func_code is obfuscated
-try:
-  print(dis.dis(main.orig_func))
-except Exception as e:
-  print(e)
-
-# Check class method is obfuscated
-try:
-  print(dis.dis(Queens.solve.orig_func))
-except Exception as e:
-  print(e)
-
 # Check __file__ of obfuscated module "queens" is filename in target machine
 import queens
 if os.path.abspath(queens.__file__) == os.path.abspath(os.path.join(os.path.dirname(__file__), "queens.py")):
   print("The value of __file__ is OK")
-  
+
 # Check __wraparmor__ can't be called out of decorator
 try:
-    from builtins import __wraparmor__
+  from builtins import __wraparmor__
 except Exception:
-    from __builtin__ import __wraparmor__
+  from __builtin__ import __wraparmor__
 try:
   __wraparmor__(main)
 except Exception as e:
   print('__wraparmor__ can not be called out of decorator')
 
 # Check filename in trackback
-queens.test_exception()
+try:
+  queens.test_exception()
+except Exception:
+  from traceback import print_exc
+  print_exc()
+
+# Access original func_code will crash: Segmentation fault
+# print(dis.dis(main.orig_func))
+# print(dis.dis(Queens.solve.orig_func))
--- tests/system-test.sh
+++ tests/system-test.sh
@@ -317,10 +317,10 @@ check_file_content $PROPATH/dist/queens.py '__pyarmor__(__name__'
 
 (cd $PROPATH/dist; $PYTHON hello.py >result.log 2>&1 )
 check_file_content $PROPATH/dist/result.log 'Found 92 solutions'
-check_file_content $PROPATH/dist/result.log 'index out of range'
 check_file_content $PROPATH/dist/result.log '__wraparmor__ can not be called out of decorator'
 check_file_content $PROPATH/dist/result.log 'The value of __file__ is OK'
 check_file_content $PROPATH/dist/result.log '<frozen queens>'
+# check_file_content $PROPATH/dist/result.log 'Segmentation fault'
 
 echo ""
 echo "-------------------- Test Use Cases END ------------------------"
