167eaf83909f06ceda9692998c6c292b6aca0e7a
Jondy Zhao <jondy.zhao@gmail.com>
2018-03-26 06:33:26 +0800

    Add function test script

---
+++ tests/function-test.sh
@@ -0,0 +1,119 @@
+source test-header.sh
+
+# ======================================================================
+#
+# Initial setup.
+#
+# ======================================================================
+
+PYARMOR="${PYTHON} pyarmor2.py"
+
+csih_inform "Python is $PYTHON"
+csih_inform "Tested Package: $pkgfile"
+csih_inform "Pyarmor is $PYARMOR"
+
+csih_inform "Make workpath ${workpath}"
+rm -rf ${workpath}
+mkdir -p ${workpath} || csih_error "Make workpath FAILED"
+
+[[ -d data ]] || csih_error "No path 'data' found in current path"
+datapath=$(pwd)/data
+
+cd ${workpath}
+[[ ${pkgfile} == *.zip ]] && unzip ${pkgfile} > /dev/null 2>&1
+[[ ${pkgfile} == *.tar.bz2 ]] && tar xjf ${pkgfile}
+cd pyarmor-$version || csih_error "Invalid pyarmor package file"
+# From pyarmor 3.5.1, main scripts are moved to src
+[[ -d src ]] && mv src/* ./
+
+csih_inform "Make path test/data"
+mkdir -p test/data
+csih_inform "Copy test files from ${datapath} to ./test/data"
+cp ${datapath}/*.py test/data
+
+csih_inform "Prepare for function testing"
+echo ""
+
+
+# ======================================================================
+#
+#  Bootstrap: help and version
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Bootstrap ---------------------------------"
+echo ""
+
+csih_inform "1. show help and import pytransform"
+$PYARMOR --help >result.log 2>&1 || csih_bug "Case 1.1 FAILED"
+[[ -f _pytransform$DLLEXT ]] || csih_error "no pytransform extension found"
+
+csih_inform "2. show version information"
+$PYARMOR --version >result.log 2>&1 || csih_bug "show version FAILED"
+
+echo ""
+echo "-------------------- Bootstrap End -----------------------------"
+echo ""
+
+# ======================================================================
+#
+#  Mode: auto-wrap
+#
+# ======================================================================
+
+echo ""
+echo "-------------------- Test Mode c-wrap --------------------"
+echo ""
+
+csih_inform "Case F-1: run obfuscated scripts with auto-wrap mode"
+if ! check_python_version_for_auto_wrap_mode ; then
+for mod_mode in none des ; do
+csih_inform "Test obf-module-mode is ${mod_mode} ..."
+PROPATH=projects/testmod_auto_wrap
+$PYARMOR init --src=test/data --entry=wrapcase.py $PROPATH >result.log 2>&1
+$PYARMOR config --obf-module-mode=${mod_mode} \
+                --obf-code-mode=wrap --disable-restrict-mode=1 \
+                --manifest="include wrapcase.py" $PROPATH >result.log 2>&1
+(cd $PROPATH; $ARMOR build >result.log 2>&1)
+
+check_file_exists $PROPATH/dist/wrapcase.py
+check_file_content $PROPATH/dist/wrapcase.py 'pyarmor_runtime'
+check_file_content $PROPATH/dist/wrapcase.py '__pyarmor__(__name__'
+
+(cd $PROPATH/dist; $PYTHON wrapcase.py >result.log 2>&1 )
+check_file_content $PROPATH/dist/result.log 'recursive call return solved'
+check_file_content $PROPATH/dist/result.log 'auto wrap mode exception'
+check_file_content $PROPATH/dist/result.log 'auto wrap mode exception'
+check_file_content $PROPATH/dist/result.log 'Segmentation fault' not
+
+csih_inform "Test obf-module-mode is ${mod_mode} END"
+done
+fi
+
+
+echo ""
+echo "-------------------- Test Mode c-wrap END ------------------------"
+echo ""
+
+
+# ======================================================================
+#
+# Finished and cleanup.
+#
+# ======================================================================
+
+# Return test root
+cd ../..
+
+echo "----------------------------------------------------------------"
+echo ""
+csih_inform "Test finished for ${PYTHON}"
+
+(( ${_bug_counter} == 0 )) || csih_error "${_bug_counter} bugs found"
+
+echo           "" \
+&& csih_inform "Remove workpath ${workpath}" \
+&& echo        "" \
+&& rm -rf ${workpath} \
+&& csih_inform "Congratulations, there is no bug found"
