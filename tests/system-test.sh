33c28a5b2417f5750709ed3643a280b7cbb7ebb1
Jondy Zhao <jondy.zhao@gmail.com>
2018-03-26 05:32:03 +0800

    Add test case for wrap mode

--- tests/system-test.sh
+++ tests/system-test.sh
@@ -259,12 +259,12 @@ echo ""
 csih_inform "Case 9.1: run benchmark test"
 for obf_module_mode in none des ; do
   csih_inform "obf_module_mode: $obf_module_mode"
-  for obf_code_mode in none des fast; do
+  for obf_code_mode in none des fast wrap ; do
     csih_inform "obf_code_mode: $obf_code_mode"
-    # cwrap mode is not stable in Python3.0/3.1/3.2
-    # if [[ $obf_code_mode == "wrap" ]] ; then
-    #     check_python_version_for_cwrap_mode && continue
-    # fi
+    # c-wrap mode is not stable in Python3.0/3.1/3.2
+    if [[ $obf_code_mode == "wrap" ]] ; then
+        check_python_version_for_cwrap_mode && continue
+    fi
     logfile="result_${obf_module_mode}_${obf_code_mode}.log"
     $PYARMOR benchmark --obf-module-mode $obf_module_mode \
                        --obf-code-mode $obf_code_mode \
@@ -326,9 +326,21 @@ check_file_content $PROPATH/dist/result.log 'The value of __file__ is OK'
 check_file_content $PROPATH/dist/result.log '<frozen queens>'
 # check_file_content $PROPATH/dist/result.log 'Segmentation fault'
 
-# csih_inform "Case T-1.3: obfuscate module with cwrap mode"
-# if ! check_python_version_for_cwrap_mode ; then
-# fi
+csih_inform "Case T-1.3: obfuscate module with cwrap mode"
+if ! check_python_version_for_cwrap_mode ; then
+PROPATH=projects/testmod_c_wrap
+$PYARMOR init --src=examples/testmod --entry=queens.py $PROPATH >result.log 2>&1
+$PYARMOR config --obf-code-mode=wrap --disable-restrict-mode=1 \
+                --manifest="include queens.py" $PROPATH >result.log 2>&1
+(cd $PROPATH; $ARMOR build >result.log 2>&1)
+
+check_file_exists $PROPATH/dist/queens.py
+check_file_content $PROPATH/dist/queens.py 'pyarmor_runtime'
+check_file_content $PROPATH/dist/queens.py '__pyarmor__(__name__'
+
+(cd $PROPATH/dist; $PYTHON queens.py >result.log 2>&1 )
+check_file_content $PROPATH/dist/result.log 'Found 92 solutions'
+fi
 
 echo ""
 echo "-------------------- Test Use Cases END ------------------------"
