5cf17cecdff00a159a1dd864bc813eac6f17883e
Jondy Zhao <jondy.zhao@gmail.com>
2018-09-17 17:10:39 +0800

    Fix pip install issue

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ import sys
 import os
 sys.path.append(os.path.dirname(__file__))
 def main():
-    from pyarmor2 import main as main_entry
+    from pyarmor import main as main_entry
     logging.basicConfig(
         level=logging.INFO,
         format='%(levelname)-8s %(message)s',
