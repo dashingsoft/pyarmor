9d40dd10d6e1d10d73f189b835603c566e788808
Jondy Zhao <jondy.zhao@gmail.com>
2018-11-04 15:26:28 +0800

    Change current path

--- src/webui/manager.bat
+++ src/webui/manager.bat
@@ -5,12 +5,20 @@ SETLOCAL
 FOR /D %%I IN (C:\Python*) DO SET PYPATH=%%I
 
 IF "%PYPATH%" == "" (
+
   ECHO No python found in C:\PythonXY.
   PAUSE
+
 ) ELSE (
+
+  ECHO Change current path to %~dp0
+  CD /D %~dp0
+
   ECHO Use %PYPATH% to run server ...
   %PYPATH%\python.exe server.py
+
   IF ERRORLEVEL 1 PAUSE
+
 )
 
 ENDLOCAL
--- src/webui/manager.sh
+++ src/webui/manager.sh
@@ -1,2 +1,3 @@
 #! /usr/bin/env bash
+cd $(dirname $0)
 python server.py
