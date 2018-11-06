44d12b55c57f28442b4e9ab9a89a5f2d0db4e0bb
Jondy Zhao <jondy.zhao@gmail.com>
2018-11-06 14:15:03 +0800

    Add prompt

--- src/examples/obfuscate-app.bat
+++ src/examples/obfuscate-app.bat
@@ -7,17 +7,20 @@ REM
 
 SETLOCAL
 
-REM TODO: Absolute path for python installed, python.exe should be here
-SET PYPATH=C:\Users\User\AppData\Local\Programs\Python\Python36-32
+REM TODO:
+SET PYTHON=C:\Python34\python.exe
+
+REM TODO: Where to find pyarmor.py
+SET PYARMOR_PATH=C:\Python34\Lib\site-packages\pyarmor
 
 REM TODO: Absolute path in which all python scripts will be obfuscated
-SET SOURCE=D:\My Workspace\Project\Src
+SET SOURCE=%PYARMOR_PATH%\examples\simple
 
 REM TODO: Entry script filename, must be relative to %SOURCE%
-SET ENTRY_SCRIPT=main.py
+SET ENTRY_SCRIPT=queens.py
 
 REM TODO: Output path for obfuscated scripts and runtime files
-SET OUTPUT=D:\My Workspace\Project\Dist
+SET OUTPUT=%PYARMOR_PATH%\examples\dist
 
 REM TODO: Let obfuscated scripts expired on some day, uncomment next line
 Rem SET LICENSE_EXPIRED_DATE=2019-01-01
@@ -26,44 +29,54 @@ REM TODO: If try to run obfuscated scripts, uncomment next line
 Rem SET TEST_OBFUSCATED_SCRIPTS=1
 
 REM Check Python
-SET PYTHON=%PYPATH%\python.exe
-IF NOT EXIST "%PYTHON%" (
-  ECHO
-  ECHO No python.exe found in %PYPATH%
-  ECHO
+%PYTHON% --version 
+IF NOT ERRORLEVEL 0 (
+  ECHO.
+  ECHO Python doesn't work, check value of variable PYTHON
+  ECHO.
   GOTO END
 )
 
 REM Check Pyarmor
-SET PYARMOR_PATH=%PYPATH%\Lib\site-packages\pyarmor
 IF NOT EXIST "%PYARMOR_PATH%\pyarmor.py" (
-  ECHO
-  ECHO No pyarmor installed, run "pip install pyarmor" to install it first
-  ECHO If pyarmor has been installed other than pip, set variable PYARMOR_PATH to the right path
-  ECHO 
+  ECHO.
+  ECHO No pyarmor found, check value of variable PYARMOR_PATH
+  ECHO. 
   GOTO END
 )
 
-REM Obfuscate scripts
+REM Check Source
+IF NOT EXIST "%SOURCE%" (
+  ECHO.
+  ECHO No %SOURCE% found, check value of variable SOURCE
+  ECHO. 
+  GOTO END
+)
+
+REM Obfuscate all the ".py" files
+ECHO.
 CD /D %PYARMOR_PATH%
 %PYTHON% pyarmor.py obfuscate --recursive --src %SOURCE% --entry %ENTRY_SCRIPT% --output %OUTPUT%
-
-REM Somehing is wrong
-IF ERRORLEVEL 1 GOTO END
+IF NOT ERRORLEVEL 0 GOTO END
+ECHO.
 
 REM Generate an expired license if any
+SET LICENSE_CODE=expired-%LICENSE_EXPIRED_DATE%
 IF DEFINED LICENSE_EXPIRED_DATE (
-  SET LICENSE_CODE=expired-%LICENSE_EXPIRED_DATE%
   %PYTHON% pyarmor.py licenses --expired %LICENSE_EXPIRED_DATE% %LICENSE_CODE%
   IF ERRORLEVEL 1 GOTO END
   
   REM Overwrite default license with this expired license
-  ECHO The obfuscated scripts will be expired on %LICENSE_EXPIRED_DATE%
+  ECHO.
+  ECHO Copy expired license to %OUTPUT%
   COPY licenses\%LICENSE_CODE%\license.lic %OUTPUT%
+  ECHO.
 )
 
 REM Run obfuscated scripts
 IF "%TEST_OBFUSCATED_SCRIPTS%" == "1" (
+  ECHO Prepare to run obfuscated script %OUTPUT%\%ENTRY_SCRIPT%
+  PAUSE
   SETLOCAL
   CD /D %OUTPUT%
   %PYTHON% %ENTRY_SCRIPT%
