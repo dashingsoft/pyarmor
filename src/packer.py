6f2d180ba89e19158053c8e558101055f79a3365
Jondy Zhao <jondy.zhao@gmail.com>
2018-11-09 16:51:34 +0800

    Refine packer

--- src/packer.py
+++ src/packer.py
@@ -46,29 +46,29 @@ try:
 except ImportError:
     from pyarmor.pyarmor import main as call_armor
 
-def packer(args):
+def armorcommand(func):
+    def wrap(*args, **kwargs):
+        old = os.getcwd()
+        os.chdir(os.path.dirname(__file__))
+        try:
+            return func(*args, **kwargs)
+        finally:
+            os.chdir(old)
+    return wrap
 
-    os.chdir(os.path.dirname(__file__))
+@armorcommand
+def _paker(src, entry, setup, packcmd, dist, libname):
+    dest = os.path.dirname(setup)
+    script = os.path.basename(setup)
 
-    src = os.path.dirname(args.entry) if args.path is None else args.path
-    entry = os.path.basename(args.entry) if args.path is None \
-        else os.path.relpath(args.entry, args.path)
-
-    project = 'build-for-%s' % args.type
-    packcmd = 'py2exe' if args.type == 'py2exe' else 'build'
-    libname = 'library.zip' if args.type == 'py2exe' else \
-        'python%s%s.zip' % sys.version_info[:2]
-
-    dist = os.path.join('build',
-                        'exe.%s-%s' % (get_platform(), sys.version[0:3])) \
-                        if args.type == 'cx_Freeze' else 'dist'
-    output = os.path.join(src, dist)
+    output = os.path.join(dest, dist)
+    project = os.path.join('projects', 'build-for-packer')
 
     options = 'init', '--type', 'app', '--src', src, '--entry', entry, project
     call_armor(options)
 
     filters = ('global-include *.py', 'prune build, prune dist',
-               'exclude %s setup.py pytransform.py' % entry)
+               'exclude %s %s pytransform.py' % (entry, script))
     options = ('config', '--runtime-path', '',  '--disable-restrict-mode', '1',
                '--manifest', ','.join(filters), project)
     call_armor(options)
@@ -80,9 +80,9 @@ def packer(args):
     shutil.copy(os.path.join(src, entry), '%s.bak' % entry)
     shutil.move(os.path.join(project, 'dist', entry), src)
 
-    p = subprocess.Popen([sys.executable, 'setup.py', packcmd], cwd=src)
+    p = subprocess.Popen([sys.executable, script, packcmd], cwd=dest)
     p.wait()
-    shutil.copy('%s.bak' % entry, os.path.join(src, entry))
+    shutil.move('%s.bak' % entry, os.path.join(src, entry))
 
     with ZipFile(os.path.join(output, libname), 'a') as f:
         f.writepy(os.path.join(project, 'dist'))
@@ -93,7 +93,25 @@ def packer(args):
     for s in os.listdir(os.path.join(project, 'runtimes')):
         if s == 'pytransform.py':
             continue
-        shutil.copy(os.path.join(project, 'runtimes', s), dist)
+        shutil.copy(os.path.join(project, 'runtimes', s), output)
+
+def packer(args):
+    bintype = 'freeze' if args.type.lower().endswith('freeze') else 'py2exe'
+
+    src = os.path.dirname(args.entry) if args.path is None else args.path
+    entry = os.path.basename(args.entry) if args.path is None \
+        else os.path.relpath(args.entry, args.path)
+    setup = os.path.join(src, 'setup.py') if args.setup is None else args.setup
+
+    dist = os.path.join(
+        'build', 'exe.%s-%s' % (get_platform(), sys.version[0:3])
+    ) if bintype == 'freeze' else 'dist'
+
+    packcmd = 'py2exe' if bintype == 'py2exe' else 'build'
+    libname = 'library.zip' if bintype == 'py2exe' else \
+        'python%s%s.zip' % sys.version_info[:2]
+
+    _packer(src, entry, setup, packcmd, dist, libname)
 
 def main(args):
     parser = argparse.ArgumentParser(
@@ -107,10 +125,10 @@ def main(args):
     parser.add_argument('-t', '--type', default='py2exe',
                         choices=('py2exe', 'py2app', 'cx_Freeze', 'PyInstaller'))
     parser.add_argument('-p', '--path', help='Source path of Python scripts')
-    parser.add_argument('-s', '--setup', default='setup.py', help='Setup script')
+    parser.add_argument('-s', '--setup', help='Setup script')
     parser.add_argument('entry', metavar='Script', nargs=1, help='Entry script')
 
-    parser.set_defaults(func=_packer)
+    parser.set_defaults(func=packer)
 
 if __name__ == '__main__':
     logging.basicConfig(
