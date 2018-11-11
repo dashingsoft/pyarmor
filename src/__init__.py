f7fe02ee5ce8ed7168e62c2493cb829985525bf6
Jondy Zhao <jondy.zhao@gmail.com>
2018-11-11 19:23:26 +0800

    Refine package

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,4 @@
 # Package pyarmor
-import logging
 import sys
-import os
-sys.path.append(os.path.dirname(__file__))
-def main_entry():
-    try:
-        from pyarmor import main
-    except ImportError:
-        from .pyarmor import main
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(levelname)-8s %(message)s',
-    )
-    main(sys.argv[1:])
+from os.path import abspath, dirname
+sys.path[0:0] = abspath(dirname(__file__))
