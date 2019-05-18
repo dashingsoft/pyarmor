791d5f66620c7cc97768033b758b2b463381348a
Jondy Zhao <jondy.zhao@gmail.com>
2019-05-18 23:37:34 +0800

    Refine test case

--- tests/function-test.sh
+++ tests/function-test.sh
@@ -597,13 +597,13 @@ echo "-------------------- Test non-ascii path --------------------"
 echo ""
 
 csih_inform "Case I18N-1: run obfuscated scripts in zh-CN path"
-dist=中文路径
-$PYARMOR obfuscate -O $dist examples/simple/queens.py  > result.log 2>&1
+output=中文路径
+$PYARMOR obfuscate -O $output/dist examples/simple/queens.py  > result.log 2>&1
 check_return_value
 
-(cd $dist; $PYTHON queens.py >result.log 2>&1 )
+(cd $output; $PYTHON dist/queens.py >result.log 2>&1 )
 check_return_value
-check_file_content $dist/result.log 'Found 92 solutions'
+check_file_content $output/result.log 'Found 92 solutions'
 
 echo ""
 echo "-------------------- Test non-ascii path END ----------------"
