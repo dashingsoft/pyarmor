a9fa24701a8e9f81c9605be3605c8c26e0081e4c
Jondy Zhao <jondy.zhao@gmail.com>
2019-11-06 10:20:24 +0800

    Refine test script

--- src/examples/testpkg/mypkg/__init__.py
+++ src/examples/testpkg/mypkg/__init__.py
@@ -1,1 +1,13 @@
+from .foo import hello
+
+
 title = 'PyArmor Test Case'
+
+
+def open_hello(msg):
+    print('This is public hello: %s' % msg)
+
+
+def proxy_hello(msg):
+    print('This is proxy hello: %s' % msg)
+    hello(msg)
