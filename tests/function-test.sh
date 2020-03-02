4662e5dcbff15e2c2f4091712a2000f90bfe9735
Jondy Zhao <jondy.zhao@gmail.com>
2020-03-02 22:44:23 +0800

    Fix testcase bug

--- tests/function-test.sh
+++ tests/function-test.sh
@@ -688,6 +688,7 @@ check_return_value
 
 cat <<EOF > $PROPATH/dist/tmain.py
 import sys
+import threading
 import foo
 
 def hello2(n):
@@ -698,13 +699,11 @@ def callback(frame, event, arg):
     print('%s:%s:%s' % (event, frame.f_code.co_name, frame.f_lineno))
     return callback
 
-import threading
 def target():
     print('foo.hello got %d' % foo.hello(2))
     print('hello2 got %d' % hello2(2))
 threading.settrace(callback)
 threading.Thread(target=target).start()
-threading.settrace(None)
 EOF
 (cd $PROPATH/dist; $PYTHON tmain.py >result.log 2>&1)
 check_return_value
