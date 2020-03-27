3551c4c3c5a1ab66855f66d81db1c8ad50b25765
Jondy Zhao <jondy.zhao@gmail.com>
2020-03-27 11:01:54 +0800

    Add another example plugin

--- plugins/foo.py
+++ plugins/foo.py
@@ -4,8 +4,16 @@ import logging
 
 
 def main():
+    logging.info("Run plugin: check_ntp_time")
     # PyArmor Plugin: check_multi_mac()
-    logging.info("Hello World!")
+
+    logging.info("Run plugin: check_ntp_time")
+    # PyArmor Plugin: check_ntp_time()
+
+    logging.info("Run plugin: check_docker")
+    # PyArmor Plugin: check_docker()
+
+    logging.info("All plugins work fine")
 
 
 if __name__ == '__main__':
