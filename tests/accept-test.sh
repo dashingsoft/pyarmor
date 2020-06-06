f8470b7b352365a4cef32db560b27206b8208cc3
Jondy Zhao <jondy.zhao@gmail.com>
2020-06-07 07:41:13 +0800

    Fix py30, py31 issue

--- tests/accept-test.sh
+++ tests/accept-test.sh
@@ -270,7 +270,8 @@ from pytransform import clean_str
 from sys import version_info as ver
 data = ('a' * 30) if ver[0] == 2 else (b'a' * 30).decode()
 clean_str(data)
-print(data)
+if (ver[0] * 10 + ver[1]) not in (30, 31):
+    print(data)
 EOF
 
 $PYARMOR obfuscate --exact -O $dist safedata.py >result.log 2>&1
@@ -512,7 +513,8 @@ from pytransform import clean_str
 from sys import version_info as ver
 data = ('a' * 30) if ver[0] == 2 else (b'a' * 30).decode()
 clean_str(data)
-print(data)
+if (ver[0] * 10 + ver[1]) not in (30, 31):
+    print(data)
 EOF
 
 $PYARMOR obfuscate --exact -O $dist safedata.py >result.log 2>&1
