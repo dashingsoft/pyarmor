6fa8edaefff9c7e7ec9237bbbed5de4dfe531a7e
Jondy <jondy.zhao@gmail.com>
2020-07-16 16:11:26 +0800

    Refine script

--- tests/vm-test.sh
+++ tests/vm-test.sh
@@ -18,15 +18,17 @@ rm -rf $TESTPATH
 mkdir -p $TESTPATH/.pyarmor
 cd $TESTPATH
 
-for pyver in 27 37 38 27-32 37-32 38-32 ; do
+TEST_VERSIONS="27 37 38 27-32 37-32 38-32"
+
+for pyver in ${TEST_VERSIONS} ; do
     PYTHON="C:/Python${pyver}/python"
     PYARMOR="$PYTHON $TESTSRC/pyarmor.py --home $TESTHOME"
 
     DIST=dist3-${pyver}
-    $PYARMOR obfuscate --advanced 3 -O $DIST $TESTSCRIPT
+    PYARMOR_HOME=$TESTHOME $PYARMOR obfuscate --advanced 3 -O $DIST $TESTSCRIPT
     (cd $DIST; $PYTHON $(basename $TESTSCRIPT) >result.log 2>&1)
 
     DIST=dist4-${pyver}
-    $PYARMOR obfuscate --advanced 4 -O $DIST $TESTSCRIPT
+    PYARMOR_HOME=$TESTHOME $PYARMOR obfuscate --advanced 4 -O $DIST $TESTSCRIPT
     (cd $DIST; $PYTHON $(basename $TESTSCRIPT) >result.log 2>&1)
 done
