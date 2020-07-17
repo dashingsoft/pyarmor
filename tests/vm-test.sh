3382d015bb56e7f787eee1ff9a230267e43a6e8a
Jondy Zhao <jondy.zhao@gmail.com>
2020-07-17 16:35:31 +0800

    Add test script

---
+++ tests/vm-test.sh
@@ -0,0 +1,32 @@
+set -x
+set -e
+
+TESTPATH=__runtest4__
+
+if [[ $(uname) == CYGWIN* ]] ; then
+    TESTHOME=$(cygpath -m $(pwd)/$TESTPATH/.pyarmor)
+    TESTSRC=$(cygpath -m $(pwd)/pyarmor/src)
+else
+    TESTHOME=$TESTPATH/.pyarmor
+    TESTSRC=$(pwd)/pyarmor/src
+fi
+TESTSCRIPT=$TESTSRC/examples/simple/queens.py
+
+[[ ! -d "$TESTSRC" ]] && echo "No pyarmor found" && exit
+
+rm -rf $TESTPATH
+mkdir -p $TESTPATH/.pyarmor
+cd $TESTPATH
+
+for pyver in 27 37 38 27-32 37-32 38-32 ; do
+    PYTHON="C:/Python${pyver}/python"
+    PYARMOR="$PYTHON $TESTSRC/pyarmor.py --home $TESTHOME"
+
+    DIST=dist3-${pyver}
+    $PYARMOR obfuscate --advanced 3 -O $DIST $TESTSCRIPT
+    (cd $DIST; $PYTHON $(basename $TESTSCRIPT) >result.log 2>&1)
+
+    DIST=dist4-${pyver}
+    $PYARMOR obfuscate --advanced 4 -O $DIST $TESTSCRIPT
+    (cd $DIST; $PYTHON $(basename $TESTSCRIPT) >result.log 2>&1)
+done
