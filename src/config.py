0dbbef1b6f7dc4f4ba2e964078396ab43801ff45
Jondy Zhao <jondy.zhao@gmail.com>
2020-12-03 18:05:50 +0800

    Increase patch number and core version

--- src/config.py
+++ src/config.py
@@ -1,9 +1,9 @@
 from sys import platform
 
-version = '6.5.5'
+version = '6.5.6'
 
 # The corresponding version of pytransform.so
-core_version = 'r37.11'
+core_version = 'r38.12'
 
 version_info = '''
 PyArmor is a command line tool used to obfuscate python scripts, bind
