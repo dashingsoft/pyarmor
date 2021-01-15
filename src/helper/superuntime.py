e44c22bf452dcb547cf723bfdf76509c29c2aabc
Jondy Zhao <jondy.zhao@gmail.com>
2021-01-15 23:56:08 +0800

    Refine code

--- src/helper/superuntime.py
+++ src/helper/superuntime.py
@@ -1,28 +1,29 @@
 import platform
+import sys
 import struct
-
-plat_table = (
-    ('windows', ('windows')),
-    ('darwin', ('darwin', 'ios')),
-    ('linux', ('linux*',)),
-    ('freebsd', ('freebsd*', 'openbsd*')),
-    ('poky', ('poky',)),
-)
-
-arch_table = (
-    ('x86', ('i386', 'i486', 'i586', 'i686')),
-    ('x86_64', ('x64', 'x86_64', 'amd64', 'intel')),
-    ('arm', ('armv5',)),
-    ('armv6', ('armv6l',)),
-    ('armv7', ('armv7l',)),
-    ('ppc64', ('ppc64le',)),
-    ('mips32', ('mips',)),
-    ('aarch32', ('aarch32',)),
-    ('aarch64', ('aarch64', 'arm64'))
-)
 
 
 def import_names():
+    plat_table = (
+        ('windows', ('windows')),
+        ('darwin', ('darwin', 'ios')),
+        ('linux', ('linux*',)),
+        ('freebsd', ('freebsd*', 'openbsd*')),
+        ('poky', ('poky',)),
+    )
+
+    arch_table = (
+        ('x86', ('i386', 'i486', 'i586', 'i686')),
+        ('x86_64', ('x64', 'x86_64', 'amd64', 'intel')),
+        ('arm', ('armv5',)),
+        ('armv6', ('armv6l',)),
+        ('armv7', ('armv7l',)),
+        ('ppc64', ('ppc64le',)),
+        ('mips32', ('mips',)),
+        ('aarch32', ('aarch32',)),
+        ('aarch64', ('aarch64', 'arm64'))
+    )
+
     plat = platform.system().lower()
     mach = platform.machine().lower()
 
@@ -49,10 +50,9 @@ def import_names():
         if bitness == 32:
             mach = 'x86'
 
-    platname = '_'.join([plat, mach])
-    name = '.'.join([__name__, platname, 'pytransform'])
+    name = '.'.join([__name__, '%s_%s' % (plat, mach), 'pytransform'])
     m = __import__(name, globals(), locals(), ['*'])
-    return m.pyarmor, m.__file__
+    sys.modules[__name__].__dict__.update(m.__dict__)
 
 
-pyarmor, __file__ = import_names()
+import_names()
