51e7c84529349cb193df416a971f4f1d7bf8cc2f
Jondy Zhao <jondy.zhao@gmail.com>
2021-09-20 17:20:13 +0800

    Refine spp inline options

--- src/sppmode.py
+++ src/sppmode.py
@@ -23,23 +23,52 @@ def mixin(obfcode, sppcode):
                     to_str(ph), to_str(sppcode), obfcode[t:]])
 
 
-def _check_ignore_option(source):
+def _check_inline_option(source):
+    options = []
+    marker = 'pyarmor options:'
     for line in source[:1024].splitlines():
         if not line.strip():
             continue
         if not line.startswith('#'):
             break
-        i = line.lower().find('pyarmor option')
-        return (i > 0) and (line.find('no-spp-mode') > 0)
+        i = line.lower().find(marker)
+        if i > 0:
+            options.extend(line[i+len(marker)].strip().split(','))
+    return set([x.strip() for x in options])
+
+
+class ExportDecorator(ast.NodeTransformer):
+
+    SPP_EXPORT_DECORATOR = '''
+def spp_export_api(f):
+
+    def wrap(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return wrap
+'''
+
+    def visit(self, node):
+        a = ast.parse(self.SPP_EXPORT_DECORATOR).body[0]
+        node.body.insert(0, a)
+        a.lineno = a.end_lineno = 1
+        a.col_offset = a.end_col_offset = 1
+        ast.fix_missing_locations(a)
 
 
 def build(source, modname, destname=None):
-    if _check_ignore_option(source):
+    options = _check_inline_option(source)
+    if 'no-spp-mode' in options:
         return None, None
+
+    mtree = ast.parse(source, modname)
+    if 'spp-export' in options:
+        ExportDecorator().visit(mtree)
 
     data = '/Users/jondy/workspace/pytransform/cmap/build/sppmode.so'
     # os.path.join('~', '.pyarmor', '_sppmode.so')
     lib = cdll.LoadLibrary(data)
     fb = PYFUNCTYPE(py_object, py_object)(('sppbuild', lib))
-    co, cox, src = fb((ast.parse(source, modname), modname))
+    co, cox, src = fb((mtree, modname))
+    print(src)
     return co, cox
