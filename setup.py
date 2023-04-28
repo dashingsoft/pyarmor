c8da6b0672730e2ac31609c8e2dd400ac53f280f
Jondy Zhao <jondy.zhao@gmail.com>
2023-04-28 08:59:46 +0800

    Increase minor version

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     },
 
     install_requires=[
-        'pyarmor.cli.core~=2.1.8'
+        'pyarmor.cli.core~=3.2.dev1'
     ],
 
     entry_points={
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from sys import platform
 
-version = '8.1.8'
+version = '8.2.dev1'
 
 # The corresponding version of pytransform.so
 core_version = 'r52.6'
