24e2e347caf9aeeb38ac34f8d3de5bd150556d2e
Jondy Zhao <jondy.zhao@gmail.com>
2023-05-03 07:51:29 +0800

    Fix bug

--- src/cli/register.py
+++ src/cli/register.py
@@ -264,7 +264,7 @@ class WebRegister(Register):
         logger.info('prepare "%s"', keyfile)
 
         rcode = self._get_old_rcode() if upgrade else None
-        if upgrade and keyfile.endswith('regcode-to-pro.txt'):
+        if upgrade and not rcode and keyfile.endswith('regcode-to-pro.txt'):
             logger.error('please use `pyarmor-7 -v` to check old license')
             logger.error('this code is used to upgrade old license')
             raise CliError('no found old license in this machine')
