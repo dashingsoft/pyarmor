930814bd2becf83efe74e7458ebce0d007012a9f
Jondy Zhao <jondy.zhao@gmail.com>
2023-05-29 09:30:23 +0800

    Increase patch number to 8.2.3

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     },
 
     install_requires=[
-        'pyarmor.cli.core~=3.2.2'
+        'pyarmor.cli.core~=3.2.3'
     ],
 
     entry_points={
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from sys import platform
 
-version = '8.2.2'
+version = '8.2.3'
 
 # The corresponding version of pytransform.so
 core_version = 'r52.6'
