635c8b97bc6443dde780b7fc859757236d619ab1
Jondy Zhao <jondy.zhao@gmail.com>
2023-06-25 16:41:14 +0800

    Refine docker auth

--- src/cli/docker.py
+++ src/cli/docker.py
@@ -5,29 +5,20 @@ import socketserver
 import struct
 import sys
 
-import docker
-
 from .context import Context
 from .generate import Builder, Pytransform3
 from .register import Register
 
-PORT = 29092
 
-
-def get_docker_gateway(client):
-    filters = {
-        'driver': 'bridge',
-        'type': 'builtin'
-    }
-    networks = client.networks.list(filters=filters)
-    return networks[0].attrs['IPAM']['Config'][0]['Gateway']
+CONFIG = {
+    'port': 29092,
+    'home': os.path.expanduser(os.path.join('~', '.pyarmor', 'docker')),
+    'machid': None,
+    'ctx': None,
+}
 
 
 class DockerAuthHandler(socketserver.BaseRequestHandler):
-
-    WORKPATH = os.path.expanduser(os.path.join('~', '.pyarmor', 'docker'))
-    CTX = None
-    CLIENT = None
 
     def handle(self):
         data = self.request.recv(64)
@@ -42,33 +33,10 @@ class DockerAuthHandler(socketserver.BaseRequestHandler):
             msg += b'\00'
             self.request.send(struct.pack('!HH', 1, len(msg)) + msg)
 
-    def copy_file_into_docker(self, containerid, filename):
-        from subprocess import check_call
-        check_call(['docker', 'cp', filename, '%s:/' % containerid])
-
-    def get_container_info(self, client, shortid):
-        try:
-            container = client.container.get(shortid)
-            return container.attrs
-        except docker.errors.NotFound:
-            pass
-        except docker.errors.APIError:
-            pass
-
     def process(self, packet):
         if packet[:4] == b'PADH':
-            self.request.send(self.MACHID)
-            return
-
-        container = self.get_container(self.client_address[0])
-        state = container.attrs['State']
-        if state.get('Status') == 'running' and state.get('Running'):
-            # 2023-06-15T23:51:03.287483366Z
-            # state.get('StartedAt')
-            # bridge = container.attrs['NetworkSettings']['Networks']['bridge']
-            # bridge['MacAddress']
-            # bridge['Gateway'] and bridge['IPAddress']
-            # bridge['IPv6Gateway'] and bridge['GlobalIPv6Address']
+            self.request.send(CONFIG['machid'])
+        else:
             userdata = self.parse_packet(packet)
             keydata = self.generate_runtime_key(userdata.decode('utf-8'))
             self.request.send(struct.pack('!HH', 0, len(keydata)) + keydata)
@@ -79,23 +47,9 @@ class DockerAuthHandler(socketserver.BaseRequestHandler):
         raise RuntimeError('invalid auth request')
 
     def generate_runtime_key(self, userdata):
-        ctx = self.CTX
+        ctx = CONFIG['ctx']
         ctx.cmd_options['user_data'] = userdata
         return Builder(ctx).generate_runtime_key()
-
-    def get_container(self, ipaddr):
-        filters = {
-            'driver': 'bridge',
-            'type': 'builtin'
-        }
-        networks = self.CLIENT.networks.list(filters=filters, greedy=True)
-        containers = networks[0].attrs.get('Containers')
-        if containers:
-            marker = ipaddr + '/'
-            for dockid, netattr in containers.items():
-                if netattr.get('IPv4Address').startswith(marker):
-                    return self.CLIENT.containers.get(dockid)
-        raise RuntimeError('no found countainer with IPv4 %s' % ipaddr)
 
 
 def register_pyarmor(ctx, regfile):
@@ -104,17 +58,13 @@ def register_pyarmor(ctx, regfile):
     reg.register_regfile(regfile)
     if reg.license_info['features'] < 15:
         raise RuntimeError('this feature is only for group license')
-
-    machid = reg._get_machine_id()
-    logging.debug('machine id: %s', machid)
-    DockerAuthHandler.MACHID = machid
-
     Pytransform3._update_token(ctx)
 
 
 def main_entry():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', type=int, default=PORT)
+    parser.add_argument('-p', '--port', type=int, default=CONFIG['port'],
+                        help=argparse.SUPPRESS)
     parser.add_argument('-s', '--sock', default='/var/run/docker.sock',
                         help=argparse.SUPPRESS)
     parser.add_argument('--home', help=argparse.SUPPRESS)
@@ -122,20 +72,19 @@ def main_entry():
                         help='group device registration file for this machine')
     args = parser.parse_args(sys.argv[1:])
 
-    home = DockerAuthHandler.WORKPATH
     if args.home:
-        DockerAuthHandler.WORKPATH = args.home
-        home = args.home
+        CONFIG['home'] = os.path.expandvars(os.path.expanduser(args.home))
+    home = CONFIG['home']
     logging.info('work path: %s', home)
 
-    ctx = Context(home=os.path.expanduser(home))
+    ctx = Context(home=home)
     register_pyarmor(ctx, args.regfile[0])
-    DockerAuthHandler.CTX = ctx
+    CONFIG['ctx'] = ctx
 
-    client = docker.from_env()
-    host, port = get_docker_gateway(client), args.port
-    DockerAuthHandler.CLIENT = client
+    CONFIG['machid'] = Pytransform3.get_hd_info(11)
+    logging.debug('machine id: %s', CONFIG['machid'])
 
+    host, port = '0.0.0.0', args.port
     with socketserver.TCPServer((host, port), DockerAuthHandler) as server:
         logging.info('listen docker auth request on %s:%s', host, args.port)
         server.serve_forever()
@@ -147,6 +96,43 @@ def main():
         format='%(asctime)s: %(message)s',
     )
     main_entry()
+
+
+#
+# Deprecrated functions
+#
+def get_docker_gateway(client):
+    filters = {
+        'driver': 'bridge',
+        'type': 'builtin'
+    }
+    networks = client.networks.list(filters=filters)
+    return networks[0].attrs['IPAM']['Config'][0]['Gateway']
+
+
+def get_container(client, ipaddr):
+    filters = {
+        'driver': 'bridge',
+        'type': 'builtin'
+    }
+    networks = client.networks.list(filters=filters, greedy=True)
+    containers = networks[0].attrs.get('Containers')
+    if containers:
+        marker = ipaddr + '/'
+        for dockid, netattr in containers.items():
+            if netattr.get('IPv4Address').startswith(marker):
+                return client.containers.get(dockid)
+    raise RuntimeError('no found countainer with IPv4 %s' % ipaddr)
+
+
+def copy_file_into_docker(containerid, filename):
+    from subprocess import check_call
+    check_call(['docker', 'cp', filename, '%s:/' % containerid])
+
+
+def get_container_info(client, shortid):
+    container = client.container.get(shortid)
+    return container.attrs
 
 
 if __name__ == '__main__':
