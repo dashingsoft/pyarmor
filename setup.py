6230cbde41c960d6396ae2327c44b82297ac8b88
Jondy Zhao <jondy.zhao@gmail.com>
2023-10-13 19:28:53 +0800

    Change version to 8.4.0

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     },
 
     install_requires=[
-        'pyarmor.cli.core~=4.3.6'
+        'pyarmor.cli.core~=4.4.0'
     ],
 
     entry_points={
--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__VERSION__ = '8.3.11'
+__VERSION__ = '8.4.0'
 
 logger = logging.getLogger('cli')
 
