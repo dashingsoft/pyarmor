44d13e5f6db8fc45c795da4e2bbf2d107e7f2abb
Jondy Zhao <jondy.zhao@gmail.com>
2024-03-08 07:20:23 +0800

    Change version to 8.5.1

--- src/cli/__init__.py
+++ src/cli/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__VERSION__ = '8.5.0'
+__VERSION__ = '8.5.1'
 
 logger = logging.getLogger('cli')
 
--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 from sys import platform
 
-version = '8.5.0'
+version = '8.5.1'
 
 # The corresponding version of pytransform.so
 core_version = 'r52.6'
