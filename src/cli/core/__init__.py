74560993c8c42ee2877dfa83351b8cfeeac88df8
Jondy Zhao <jondy.zhao@gmail.com>
2024-05-27 10:49:22 +0800

    Auto install prebuilt package for other archs

--- src/cli/core/__init__.py
+++ src/cli/core/__init__.py
@@ -23,58 +23,21 @@
 __VERSION__ = '6.5.2'
 
 
-def format_platform():
-    import platform
-    import sys
-    from struct import calcsize
+def check_and_install_prebuilt_package():
+    import os
+    from pyarmor.cli.context import format_platform
+    from pyarmor.cli.bootstrap import check_prebuilt_runtime_library
+    from .runtime import PLATFORM_NAMES
 
-    def format_system():
-        plat = platform.system().lower()
-        plat = ('windows' if plat.startswith('cygwin') else
-                'linux' if plat.startswith('linux') else
-                'freebsd' if plat.startswith(
-                    ('freebsd', 'openbsd', 'isilon onefs')) else plat)
-        if plat == 'linux':
-            if hasattr(sys, 'getandroidapilevel'):
-                plat = 'android'
-            else:
-                cname, cver = platform.libc_ver()
-                if cname == 'musl':
-                    plat = 'alpine'
-                elif cname == 'libc':
-                    plat = 'android'
-        return plat
+    plat, arch = format_platform()
+    platname = '%s.%s' % (plat, arch)
+    if platname not in PLATFORM_NAMES:
+        raise RuntimeError('"%s" is still not supported by Pyarmor' % platname)
 
-    def format_machine():
-        mach = platform.machine().lower()
-        arch_table = (
-            ('x86', ('i386', 'i486', 'i586', 'i686')),
-            ('x86_64', ('x64', 'x86_64', 'amd64', 'intel')),
-            ('arm', ('armv5',)),
-            ('armv6', ('armv6l',)),
-            ('armv7', ('armv7l',)),
-            ('aarch32', ('aarch32',)),
-            ('aarch64', ('aarch64', 'arm64')),
-            ('ppc64le', ('ppc64le',)),
-            ('mips32el', ('mipsel', 'mips32el')),
-            ('mips64el', ('mips64el',)),
-            ('riscv64', ('riscv64',)),
-        )
-        for alias, archlist in arch_table:
-            if mach in archlist:
-                mach = alias
-                break
-        return mach
+    if not os.path.exists(os.path.join(os.path.dirname(__file__), plat, arch)):
+        check_prebuilt_runtime_library([plat])
 
-    plat, mach = format_system(), format_machine()
-    if plat == 'windows' and mach == 'x86_64':
-        bitness = calcsize('P'.encode()) * 8
-        if bitness == 32:
-            mach = 'x86'
-    elif plat == 'darwin' and mach == 'aarch64':
-        mach = 'arm64'
-
-    return plat, mach
+    return plat, arch
 
 
 def _import_pytransform3():
@@ -84,7 +47,7 @@ def _import_pytransform3():
             fromlist=('__pyarmor__',), level=1
         )
     except ModuleNotFoundError:
-        plat, arch = format_platform()
+        plat, arch = check_and_install_prebuilt_package()
         modname = '.'.join([plat, arch, 'pytransform3'])
         return __import__(
             modname, globals=globals(), locals=locals(),
